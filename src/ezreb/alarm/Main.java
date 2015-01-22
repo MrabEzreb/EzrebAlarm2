12faa6048b2957c96bdf47dff602103115d79994
MrabEzreb <de013b3f@opayq.com>
2015-01-22 13:05:54 -0500

    Testing this crazy commiting thing, :D

--- src/ezreb/alarm/Main.java
+++ src/ezreb/alarm/Main.java
@@ -27,6 +27,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//testing
 		if(alarmData.exists() == false) {
 			alarmData.mkdirs();
 		}
